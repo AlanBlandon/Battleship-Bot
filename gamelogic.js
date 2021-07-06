98bb778d07169f13bd3b316fc62d38b2e1d5f5c8
barrotbake <nguyenjacob411@gmail.com>
2021-07-05 22:40:48 -0700

    Mades some logic functions 😳 and a ship object 🚢

---
+++ gamelogic.js
@@ -0,0 +1,23 @@
+// Use this function to check the validity of the hits
+// This accepts the letter and number for the cords and a 2d array of the targets board
+let didHit = (targetSquareNum, targetSquareLetter, targetGrid) => {
+    if(targetGrid[targetSquareNum][targetSquareLetter]){
+        return true
+    }
+        return false
+}
+
+//Example of a ship object
+//It just has two arrays to show the ships cords and if they have been hit
+let exampleShip = {
+    cords:["A1", "B2", "C3"],
+    hit:[true, true, true]
+}
+
+// Use this function to check if a ship has been sunk
+// This function accepts the a ship object like the example above
+let shipSunk = ship => {
+    return ship.hit.every(cord => { 
+        return cord 
+    })  
+}
